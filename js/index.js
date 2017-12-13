1e4c51cb093a239054ccb170bd381ddc15e2c8b2
Anastasiya Razumeyko <anastasiya.razumeyko@gmail.com>
2017-12-13 15:04:13 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.querySelectorAll(.number)forEach(el=> {
-    el.addEventListener('click',function(dis) {
-        const display = document.getElementsByClassName(Show);
+document.querySelectorAll('.number').forEach(el=> {
+    el.addEventListener('click', function(dis) {
+        const display = document.getElementByClassName('.show');
         const number = dis.target.innerHtml;
         display.innerHtml += number;
     })
